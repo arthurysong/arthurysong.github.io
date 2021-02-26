89f5f4496c51d7128804a1505ff240d87c2ad6a8
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-02-25 22:29:44 -0800

    Update settings of project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Paper } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "@fontsource/ibm-plex-mono"
+import mwriterdemo from "../images/demo.gif"
+import cpdemo from "../images/cp-demo.gif"
+
 
 const IndexPage = () => (
-  <Layout>
+  <div style={{ width: 1220, margin: '40px auto', fontFamily: "IBM Plex Mono" }}>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>Arthur Song</h1>
+    <p>Recent Projects</p>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Project img={mwriterdemo} imgAlt="mwriter demo gif" name="MWriter" description="A journaling application that can publish entries directly to Medium's website. Uses GitHub API to automatically create and import GitHub gists during publishing. ReactJS, MongoDB, Express, GitHub & Medium API"/>
+      <Project img={cpdemo} imgAlt="console poker demo gif" name="Console Poker" description="Retro 8-bit themed Poker application. Users can deposit real money using Stripe into their accounts and play live Poker. Ruby on Rails, WebSockets, Redux Middlewares, React, and Stripe API, Google OAuth"/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
+)
+
+const Project = ({ img, imgAlt, name, description }) => (
+  <Paper style={{ display: 'inline-block', width: 500, marginBottom: 32 }}>
+    <img src={img} alt={imgAlt} width={500}/>
+    <p style={{ padding: '0 14px' }}><strong>{name}</strong> - {description}</p>
+  </Paper>
 )
 
 export default IndexPage
