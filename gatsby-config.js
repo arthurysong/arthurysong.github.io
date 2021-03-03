72eb4ef2b2c03ec92604b80e226d720fcfc4fac3
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-03-02 21:29:31 -0800

    New favi

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  pathPrefix: "/arthurysong.github.io"
+  // pathPrefix: "/arthurysong.github.io"
 }
